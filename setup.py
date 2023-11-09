fee22b93915e29e54fbea8fe6d72c7c66bbfd85c
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2023-11-09 16:37:57 +0100

    test

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=INSTALL_REQUIRES,
     entry_points={
         "console_scripts": [
+            "upload-build=dana_client.upload_build:main",
             "publish-build=dana_client.publish_build:main",
             "publish-backup=dana_client.publish_backup:main",
             "update-project=dana_client.update_project:main",
